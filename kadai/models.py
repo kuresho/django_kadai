59987a4fee0d23b00e7126764f4f5fbdf64bbb43
user <kure5257@gmail.com>
2024-05-28 09:59:41 +0900

    no message

--- kadai/models.py
+++ kadai/models.py
@@ -1,3 +1,10 @@
+from accounts.models import CustomUser
 from django.db import models
 
-# Create your models here.
+class kadai(models.Model):
+    """課題モデル"""
+
+    user = models.ForeignKey(CustomUser,verbose_name='ユーザー',on_delete=models.PROTECT)
+    year = models.SmallAutoField(verbose_name='年',null=False)
+    month = models.SmallAutoField(verbose_name='月',null=False)
+    word = models.TextField(verbose_name='検索ワード',blank=True,null=True)
