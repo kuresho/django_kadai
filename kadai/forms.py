3484a70a639c04a8818a600f35ad865e5c6875ea
user <kure5257@gmail.com>
2024-05-31 15:19:38 +0900

    no message

--- kadai/forms.py
+++ kadai/forms.py
@@ -3,8 +3,8 @@ from django import forms
 import datetime
 
 class SearchForm(forms.ModelForm):
-    year = forms.ChoiceField(choices=[(year, year) for year in range(2018, datetime.datetime.now().year + 1)])
-    month = forms.ChoiceField(choices=[(month, month) for month in range(1, 13)])
+    year = forms.ChoiceField(label='年',choices=[(year, year) for year in range(2018, datetime.datetime.now().year + 1)])
+    month = forms.ChoiceField(label='月',choices=[(month, month) for month in range(1, 13)])
 
     class Meta:
         model = Kadai
--- kadai/models.py
+++ kadai/models.py
@@ -5,7 +5,7 @@ class Kadai(models.Model):
     user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
     year = models.CharField(verbose_name='年', max_length=4)
     month = models.CharField(verbose_name='月', max_length=2)
-    words = models.TextField(verbose_name='検索ワード', blank=True, null=True)
+    words = models.CharField(verbose_name='検索ワード', blank=True, null=True)
     search_at = models.DateTimeField(verbose_name='検索日時', auto_now=True)
 
     class Meta:
